2072da4899c1cef6992068448c45291e5ae235e0
Denis PR <na_namka@vk.com>
2026-04-01 20:28:35 +0300

    Реализована асинхронная загрузка статей из Wikipedia с помощью пакета http.

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -7,7 +8,7 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
@@ -17,21 +18,39 @@ void main(List<String> arguments) {
 
 void printUsage() {
   print(
-    "Доступные команды: 'help', 'version', 'search <НАЗВАНИЕ-СТАТЬИ>'"
+    "Доступные команды: 'help', 'version', 'wikipedia <НАЗВАНИЕ-СТАТЬИ>'"
   );
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
   if (arguments == null || arguments.isEmpty) {
     print('Пожалуйста, введите название статьи.');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('Название статьи не введено. Выход.');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
 
-  print('Ищем статьи о "$articleTitle". Пожалуйста, подождите.');
-  print('Вот что нашлось:');
-  print('(Здесь будет настоящая статья о "$articleTitle")');
+  print('Ищем статью о "$articleTitle". Пожалуйста, подождите.');
+  final articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body; // JSON строка
+  }
+  return 'Ошибка: не удалось загрузить статью "$articleTitle". Код статуса: ${response.statusCode}';
 }
