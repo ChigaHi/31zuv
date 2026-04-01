3b1200fe66d1149f0ea89a0fe149e52f09080174
Denis PR <na_namka@vk.com>
2026-04-01 20:58:43 +0300

    Создана объектно-ориентированная структура команд: абстрактный Argument, классы Option и Command

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
